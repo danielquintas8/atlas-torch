744a2f983bc4cdc3082a114f01bf842e7401f87c
danielquintas8 <daniel.quintas.lopes@gmail.com>
2026-04-07 11:01:23 +0100

    fix: disable per_parameter_lr_modulation with omega (shape mismatch)

--- train_mac.py
+++ train_mac.py
@@ -132,6 +132,7 @@ if USE_ATLAS:
     neural_memory_kwargs.update(NeuralMemory.atlas_config())
     NEURAL_MEM_SEGMENT_LEN = max(NEURAL_MEM_SEGMENT_LEN, neural_memory_kwargs.get('omega_context', 1))
     neural_memory_kwargs['attn_pool_chunks'] = False  # omega rule uses per-token params, no chunk pooling
+    neural_memory_kwargs['per_parameter_lr_modulation'] = False  # TODO: fix shape mismatch with omega per-token grads
 
     assert NEURAL_MEM_BATCH_SIZE % NEURAL_MEM_SEGMENT_LEN == 0, \
         f'NEURAL_MEM_BATCH_SIZE ({NEURAL_MEM_BATCH_SIZE}) must be divisible by NEURAL_MEM_SEGMENT_LEN ({NEURAL_MEM_SEGMENT_LEN})'
